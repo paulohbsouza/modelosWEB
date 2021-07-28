f35d67f73b0d9603c3618feeec4ab806116add9a
Paulo H B Souza <42076064+paulohbsouza@users.noreply.github.com>
2021-07-28 19:44:51 -0300

    Initial commit

---
+++ sass/sass-lang/index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="./dist/css/reset.css">
+  <link rel="stylesheet" href="./dist/css/style.css">
+
+  <title>AdminTheme - Diretriz</title>
+</head>
+
+<body>
+
+
+
+  <div class="container">
+    <article role="main">
+      article
+    </article>
+    <aside role="complementary">
+      aside
+    </aside>
+  </div>
+  <div>
+    <img src="./img/logo.png" />
+  </div>
+  <div class="teste">
+    <h1>Hello, world!</h1>
+    <p class="info1">ASDASD asdasdA ASDA ASD ASDASD</p>
+    <p class="alert1">ASDASD asdasdA ASDA ASD ASDASD</p>
+    <p class="success1">ASDASD asdasdA ASDA ASD ASDASD</p>
+  </div>
+
+  <div class="message">mensagem</div>
+  <div class="success">mensagem</div>
+  <div class="error">mensagem</div>
+  <div class="warning">mensagem</div>
+
+
+
+
+
+
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>

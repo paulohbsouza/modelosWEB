57e0a6b5ef70bd1e187913a93aab27d0bbda66b0
Paulo H B Souza <42076064+paulohbsouza@users.noreply.github.com>
2022-01-10 08:29:54 -0300

    react

--- javascript-jr/app1/src/App.js
+++ javascript-jr/app1/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './App.css';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 class App extends React.Component{
     render(){
         return(
-            <p>Tremm.</p>
+            <div>
+                <p className="cor-tex">Tremm.</p>
+            </div>
         )
     }
 }

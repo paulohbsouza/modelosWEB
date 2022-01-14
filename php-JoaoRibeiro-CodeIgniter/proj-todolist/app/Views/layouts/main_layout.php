acded6c0714c1f789b603c2c82dfca0ac1063c3b
Paulo H B Souza <42076064+paulohbsouza@users.noreply.github.com>
2022-01-14 17:37:08 -0300

    atualizações...

---
+++ php-JoaoRibeiro-CodeIgniter/proj-todolist/app/Views/layouts/main_layout.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="<?= base_url('assets/bootstrap.min.css') ?>" />
+    <title>Todo List!</title>
+</head>
+<body>
+
+    <?= $this->renderSection('content') ?>
+
+    <script src="<?= base_url('assets/jquery.slim.min.js') ?>"></script>
+    <script src="<?= base_url('assets/popper.min.js') ?>"></script>
+    <script src="<?= base_url('assets/bootstrap.min.js') ?>"></script>
+
+</body>
+</html>

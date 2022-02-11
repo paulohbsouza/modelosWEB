3763f0e9ec2b90df27a1eb5d1fed71727c0b7a08
Paulo H B Souza <42076064+paulohbsouza@users.noreply.github.com>
2022-02-10 22:50:48 -0300

    Update index.php

--- php-UpinsideTreinamentos/fsphp/03-manipulacao-e-tratamento/03-04-manipulacao-de-objetos/index.php
+++ php-UpinsideTreinamentos/fsphp/03-manipulacao-e-tratamento/03-04-manipulacao-de-objetos/index.php
@@ -7,8 +7,28 @@ fullStackPHPClassName("03.04 - Manipulação de objetos");
  */
 fullStackPHPClassSession("manipulação", __LINE__);
 
+$arrProfile = [
+    "name" => "Robson",
+    "company" => "UpInside",
+    "mail" => "cursos@upinside.com.br"
+];
+$objProfile = (object)$arrProfile;
+var_dump($arrProfile, $objProfile);
+
+echo "<p>{$arrProfile['name']} trabalha na {$arrProfile['company']}</p>";
+echo "<p>{$objProfile->name} trabalha na {$objProfile->company}</p>";
 
 /**
  * [ análise ] class | objetcs | instances
  */
 fullStackPHPClassSession("análise", __LINE__);
+
+$date = new DateTime('now', new DateTimeZone('America/Sao_Paulo'));
+
+var_dump([
+    "class" => get_class($date),
+    "methods" => get_class_methods($date),
+    "vars" => get_object_vars($date),
+    "parent" => get_parent_class($date),
+    "subclass" => is_subclass_of($date, 'DateTime')
+]);

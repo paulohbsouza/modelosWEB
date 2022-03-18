0f01838016148780a4664977f5219055e71b3dff
Paulo H B Souza <42076064+paulohbsouza@users.noreply.github.com>
2022-03-18 19:45:01 -0300

    atualização projeto

--- javascript-ReactAdminDashboard/react-dashboard/src/components/sidebar/Sidebar.js
+++ javascript-ReactAdminDashboard/react-dashboard/src/components/sidebar/Sidebar.js
@@ -7,7 +7,7 @@ const Sidebar = ({ sidebarOpen, closeSidebar }) => {
             <div className="sidebar__title">
                 <div className="sidebar__img">
                     <img src={logo} alt="logo"/>
-                    <h1>Codersbite</h1>
+                    <h1> Codersbite</h1>
                 </div>
                 <i className="fa fa-times" id="sidebarIcon" onClick={() => closeSidebar()}></i>
             </div>

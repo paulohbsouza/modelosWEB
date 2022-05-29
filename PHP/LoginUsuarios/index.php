4477ae6891fc11841ce35431364dbef34346251b
Paulo H B Souza <42076064+paulohbsouza@users.noreply.github.com>
2022-05-29 20:40:23 -0300

    new

---
+++ PHP/LoginUsuarios/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="./assets/fontawesome/css/all.min.css" rel="stylesheet">
+
+    <title>Document</title>
+</head>
+
+<body>
+
+    <i class="fa-solid fa-address-book"></i>
+    <i class="fa-regular fa-address-book"></i>
+    <i class="fa-brands fa-amazon"></i>
+    
+</body>
+
+</html>

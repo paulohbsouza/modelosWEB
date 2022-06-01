04e9b7a98e1f6c5f55051aaefb16e7c9387e57a5
Paulo H B Souza <42076064+paulohbsouza@users.noreply.github.com>
2022-06-01 16:49:53 -0300

    Update zz_empty.php

--- zz_scripts/zz_empty.php
+++ zz_scripts/zz_empty.php
@@ -1,2 +1,10 @@
 <?php
 
+echo ceil(4.3) . "<br/>";    // 5
+echo ceil(9.999) . "<br/>";  // 10
+echo ceil(-3.14) . "<br/>";  // -3
+
+
+echo round(11.4997, 3) . "<br/>";
+echo round(10.0026, 3) . "<br/>";
+

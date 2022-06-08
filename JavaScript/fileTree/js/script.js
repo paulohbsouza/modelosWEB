0ebc5d4937d59dc2db01f2b30f8fb17a2503b8f5
Paulo H B Souza <42076064+paulohbsouza@users.noreply.github.com>
2022-06-08 17:55:00 -0300

    new

---
+++ JavaScript/fileTree/js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+	$(document).ready(function() {
+
+		//carrega o file-tree
+		$(".nav-tree-list").filetree({
+			collapsed: false
+		});
+
+		/*
+		var janela = $(document).outerHeight();
+		$(".j_widget").css("height", janela);
+		*/
+	});
+});

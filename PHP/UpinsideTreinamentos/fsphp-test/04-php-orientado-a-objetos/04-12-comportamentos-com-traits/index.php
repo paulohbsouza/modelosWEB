ab726ff45d2f9119219051228d1aa09751a44a6c
paulohbsouza <paulohbsouza@hotmail.com>
2022-07-05 21:10:02 -0300

    traits

--- PHP/UpinsideTreinamentos/fsphp-test/04-php-orientado-a-objetos/04-12-comportamentos-com-traits/index.php
+++ PHP/UpinsideTreinamentos/fsphp-test/04-php-orientado-a-objetos/04-12-comportamentos-com-traits/index.php
@@ -9,3 +9,32 @@ require __DIR__ . "/source/autoload.php";
  * do objeto (BEHAVES LIKE). http://php.net/manual/pt_BR/language.oop5.traits.php
  */
 fullStackPHPClassSession("trait", __LINE__);
+
+$user = new \Source\Traits\User("Paulo", "Souza", "email@email.com");
+$address = new \Source\Traits\Address("Nome da rua", 354, "CasaB");
+
+$register = new \Source\Traits\Register(
+    $user,
+    $address
+);
+
+var_dump(
+    $register,
+    $register->getUser(),
+    $register->getAddress(),
+    $register->getUser()->getFirstName(),
+    $register->getAddress()->getStreet()
+);
+
+$cart = new \Source\Traits\Cart();
+
+$cart->add(1,"Full Stack PHP Developer", 1, 2000);
+$cart->add(2,"Laravel Developer", 2, 1000);
+$cart->add(3, "WS PHP", 5, 500);
+
+$cart->remove(2, 1);
+$cart->remove(3, 5);
+
+$cart->checkout($user, $address);
+
+var_dump($cart);
